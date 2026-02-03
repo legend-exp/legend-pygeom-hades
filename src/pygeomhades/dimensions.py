7baf10e29845abbdce7a5579905ccaa730dd1d73
Toby Dixon <toby.dixon.23@ucl.ac.uk>
2026-02-03 11:48:25 +0000

    fix the source holder

--- src/pygeomhades/dimensions.py
+++ src/pygeomhades/dimensions.py
@@ -220,7 +220,7 @@ def get_source_metadata(source_type: str, meas_type: str = "") -> AttrsDict:
     return AttrsDict(source)
 
 
-def get_source_holder_metadata(source_type: str, meas_type: str) -> AttrsDict:
+def get_source_holder_metadata(source_type: str, meas_type: str = "lat") -> AttrsDict:
     """Get the dimensions of the source holder.
 
     Parameters
@@ -274,7 +274,7 @@ def get_source_holder_metadata(source_type: str, meas_type: str) -> AttrsDict:
         if meas_type == "lat":
             source_holder.outer_width = 181.6
             source_holder.inner_width = 101.6
-            source_holder.lat = AttrsDict({"height": 65.0, "cavity_height": 60.0, "cavity_width": 50.0})
+            source_holder["lat"] = AttrsDict({"height": 65.0, "cavity_height": 60.0, "cavity_width": 50.0})
     else:
         msg = f"Source must be co, ba, am_collimated, am or th not {source_type}"
         raise RuntimeError(msg)
