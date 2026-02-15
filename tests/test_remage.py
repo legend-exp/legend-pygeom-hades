fbd38cb25fa190cbc1171fa8a2049ee91371e267
Toby Dixon <toby.dixon.23@ucl.ac.uk>
2026-02-15 12:54:47 +0000

    check overlaps for all dets (#27)
    * check overlaps for all dets
    
    * style: pre-commit fixes
    
    * test
    
    * add daq settings
    
    * only check all with public geom
    
    * fixes
    
    * add a functionality to plot the profiles
    
    * style: pre-commit fixes
    
    * test coverage
    
    * Update src/pygeomhades/plot.py
    
    Co-authored-by: Luigi Pertoldi <gipert@pm.me>
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Luigi Pertoldi <gipert@pm.me>

--- tests/test_remage.py
+++ tests/test_remage.py
@@ -1,13 +1,17 @@
 from __future__ import annotations
 
 import os
+from importlib import resources
 from pathlib import Path
 
 import dbetto
 import pygeomtools
 import pytest
+from dbetto import AttrsDict
+from pyg4ometry import geant4
 
 from pygeomhades import core
+from pygeomhades.core import construct
 
 public_geom = os.getenv("LEGEND_METADATA", "") == ""
 
@@ -37,6 +41,32 @@ def gdml_files(tmp_path):
         pygeomtools.write_pygeom(reg, gdml_file)
         out.append(gdml_file)
 
+    dets = Path(resources.files("pygeomhades") / "configs" / "holder_wrap").glob("*.yaml")
+
+    # test for all detectors
+    # only works for public geometry
+    for det in dets:
+        if public_geom:
+            continue
+
+        reg = construct(
+            AttrsDict(
+                {
+                    "detector": str(det.stem),
+                    "campaign": "c1",
+                    "measurement": "am_HS1_top_dlt",
+                    "daq_settings": AttrsDict({"flashcam": {"card_interface": "efb2"}}),
+                }
+            ),
+            public_geometry=public_geom,
+        )
+        assert isinstance(reg, geant4.Registry)
+
+        gdml_file = tmp_path / f"hades-public-{det.stem}.gdml"
+
+        pygeomtools.write_pygeom(reg, gdml_file)
+        out.append(gdml_file)
+
     return out
 
 
@@ -48,4 +78,9 @@ def test_overlaps(gdml_files):
         "/run/initialize",
     ]
     for gdml_file in gdml_files:
-        remage_run(macro, gdml_files=str(gdml_file), raise_on_error=True, raise_on_warning=True)
+        remage_run(
+            macro,
+            gdml_files=str(gdml_file),
+            raise_on_error=True,
+            raise_on_warning=True,
+        )
